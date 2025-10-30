fd12a8de8d393acd0943a93a87370a811b66f7a8
shariq63 <shariqrahman03@gmail.com>
2025-10-30 19:11:52 +0530

    Minor Chnges

--- Commercenest/web/src/app/terms-of-service/page.tsx
+++ Commercenest/web/src/app/terms-of-service/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
-export default function TermsAndConditions() {
+export default function TermsofService() {
   const router = useRouter();
 
   const handleGoBack = () => {
